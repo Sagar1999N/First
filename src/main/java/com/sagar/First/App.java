722bd7e074f7f0bae9a9b7810c217a240e3f1343
Sagar1999N <sagarnalawade555@gmail.com>
2025-08-19 20:59:45 +0530

    First Commit

---
+++ src/main/java/com/sagar/First/App.java
@@ -0,0 +1,34 @@
+package com.sagar.First;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.hadoop.util.VersionInfo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.setProperty("hadoop.home.dir", "C:\\hadoop");
+		SparkSession sp = SparkSession.builder().master("local[*]").appName("first_app").getOrCreate();
+		System.out.println(sp.version());
+		System.out.println(VersionInfo.getVersion());
+		System.out.println(sp.sparkContext().appName());
+		Dataset<Row> employees = sp.read().option("header", true).csv("./resources/in/employees.csv");
+		employees = employees.withColumn("salary", functions.col("salary").cast(DataTypes.IntegerType));
+		employees.show(10, false);
+		employees.printSchema();
+		Dataset<Row> RichEmployees = employees.filter(functions.col("salary").gt(5000));
+		RichEmployees.show(10, false);
+		Dataset<Row> GroupedSalary = employees.groupBy("department").agg(functions.avg("salary").as("avg_salary"));
+		GroupedSalary.show(10, false);
+		GroupedSalary.write().mode(SaveMode.Overwrite).parquet("./resources/out");
+		System.out.println("writing complete");
+	}
+}
